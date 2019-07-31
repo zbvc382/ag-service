2d0b1c2d231723b97c84663d05eb9a579485df52
zbvc382 <pocius.zilvinas@gmail.com>
2019-07-31 14:47:58 +0300

    remove mat list services nav menu and replace with css alt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { MatSidenavModule, MatListModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
 import { HomeModule } from './home/home.module';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 import { AppliancesModule } from './appliances/appliances.module';
@@ -34,7 +33,6 @@ import { MetaService } from './_services/meta.service';
     MatSidenavModule,
     BrowserAnimationsModule,
     MatListModule,
-    MatMenuModule,
     HomeModule,
     AppliancesModule,
     PriceListModule,
